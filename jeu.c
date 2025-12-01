3676aa12c15531296cf806869e22aa50966dd95c
rubenmandel2006-sketch <ruben.mandel.2006@gmail.com>
2025-12-01 20:10:48 +0100

    Fonction initialisation plateau + remplissage cases vide + affichage dans terminal

--- jeu.c
+++ jeu.c
@@ -1,2 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+#define P 10
 
+
+void initialisationplateau(int tab[N][P]) {
+	int i, j;
+	for (i = 0; i < N; i++) {
+        // boucle pour parcourir "les colonnes" de la ligne i
+        for (j = 0; j < P; j++) {
+        	tab[i][j] = 0;
+        }
+    }
+}
+
+void remplissagecasesvides(int tab[N][P]) {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < P; j++) {
+        	if (tab[i][j]==0) {
+        		tab[i][j] = rand() % 6;
+        	}
+        }
+    }
+}
+
+void afficherplateauterminal(int tab[N][P]) {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < P; j++) {
+            printf("%3d ", tab[i][j]);
+        }
+        printf("\n");
+    }
+}
