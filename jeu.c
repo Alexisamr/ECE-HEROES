7633e04027c840dccdbbe2a83acdaddf8183a493
rubenmandel2006-sketch <ruben.mandel.2006@gmail.com>
2025-12-05 10:07:42 +0100

    Refactor detection functions for horizontal and vertical checks

--- jeu.c
+++ jeu.c
@@ -65,23 +65,50 @@ void itemspeciaux(int tab[N][J]) {
     }
 }
 
-void detectionsuite4horizontale(int tab[N][P]) {
-	int i, j;
-	for (i = 0; i < N; i++) {
-        for (j = 0; j < P; j++) {
-        	tab[i][j] = 0;
+void detectionsuite4horizontale(int tab[N][J]) {
+	for (int i=0; i<N; i++) {
+        int compteur=1;
+		for (int j=1; j<J; j++) {
+			if (tab[i][j]==tab[i][j-1]) {
+                compteur++;
+				if (count==4) {
+                    tab[i][j]=0;
+                    tab[i][j-1]=0;
+                    tab[i][j-2]=0;
+                    tab[i][j-3]=0;
+
+                    compteur();  
+                    compteur = 1;   
+                }
+
+            } else {
+                compteur = 1;
+            }
+        }
+    }
+}
+void detectionSuite4Verticale(int tab[N][J]) {
+	for (int j=0; j<J; j++) {        
+        int compteur=1;
+		for (int i=1; i<N; i++) {   
+			if (tab[i][j]==tab[i-1][j]) {
+                compteur++;
+				if (compteur==4) {
+					tab[i][j]=0;
+                    tab[i-1][j]=0;
+                    tab[i-2][j]=0;
+                    tab[i-3][j]=0;
+					compteur(); 
+					compteur=1;
+                }
+				} else {
+                compteurIdentiques = 1;
+            }
         }
     }
 }
 
-void detectionsuite4verticale(int tab[N][P]) {
-	int i, j;
-	for (i = 0; i < N; i++) {
-        for (j = 0; j < P; j++) {
-        	tab[i][j] = 0;
-        }
-    }
-}
+
 
 void detectioncroix9(int tab[N][P]) {
 	int i, j;
@@ -100,5 +127,4 @@ void detectioncarre4par4(int tab[N][P]) {
         }
     }
 }
-
 
