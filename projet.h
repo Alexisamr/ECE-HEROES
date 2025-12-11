dcbfd82366cca826f71cacbebf3d4b4ce500a0c8
Alexis Amouri <alexis.amouri@edu.ece.fr>
2025-12-11 16:19:17 +0100

    LE HEEADERS

--- projet.h
+++ projet.h
@@ -1,1 +1,29 @@
+#ifndef PROJET_H
+#define PROJET_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#define LIGNES 25
+#define COLONNES 45
+
+typedef struct { //ça c'est pour toi ruben
+  int grille[LIGNES][COLONNES];
+  int score;
+  int vies;
+  int temps_restants;
+  int coups_restants:
+  int niveau_actuel;
+}
+
+// TOUTES LES FONCTIONS 
+
+void afficherMenu();
+void afficherTitre();
+void afficherRegles();
+void afficherTemps(t_jeu jeu);
+void afficherVieJoueur(t_jeu jeu);
+void actualiser_grille(t_jeu jeu);
+
+#endif
