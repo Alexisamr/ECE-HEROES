7fe4d2856fa8e22d1c8be7ad8998aca444893db1
Nico_rte <nicolas.rateau@edu.ece.fr>
2025-12-15 09:27:33 +0100

    fonction du choix du mode de jeu (reste le switch a mettre)

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 main{
+    int choix;
+	do{
+		choix = affichermenu();
+        //mettre le switch pour jouer ici (switch 1 : afficher regle, switch 2 -> toutes les fonctions pour jouer la partie)
+	}while(choix != 0);
+	return 0;
+	
+
 
 }
 
